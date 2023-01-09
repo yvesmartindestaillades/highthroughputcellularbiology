5f1f7d2abd27838d9319c09220b90db0c7433c70
Yves Martin <59341242+yvesmartindestaillades@users.noreply.github.com>
2023-01-09 12:30:32 -0800

    b/i and b/ii work

---
+++ src/generate_dataset.py
@@ -0,0 +1,14 @@
+
+
+def generate_barcode_df(df):
+    """Genreates a dataframe to study the impact of barcodes on the population average.
+
+    Args:
+        df (pd.Dataframe): study dataframe
+
+    Returns:
+        df: a dataframe with the following columns:
+            | sample | pearson_score | construct_1 | construct_2 | section_1 | section_2 | barcode_1 | barcode_2 | mut_rates_1 | mut_rates_2 | ...
+    """
+    
+    return 0
